8e39235bd531407179af8678a5af99dc6131fa6e
MHeMoTexHuK <daytimer253@gmail.com>
2021-08-30 23:20:59 +0300

    balancing & visual tweaks. i have an idea. it's dumb but... I'll try to implement it anyways

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-function SkatBiteAbilityLambda(damage_, reload_) {
+function SkatBiteAbilityLambda(damage_, reload_, angle_) {
 	return extend(Ability, {
-		name: "Skat Bite",
 		damage: damage_,
 		reload: reload_,
+		angle: angle_ / 2,
 		reloadTimer: 0,
 		
-		effect: Fx.shootSmall,
+		effect: Fx.generate,
 		
 		update(skat) {
-			if (this.reloadTimer++ < this.reload) return;
+			if ((this.reloadTimer += Time.delta) < this.reload) return;
 			this.reloadTimer = 0; 
 			
 			Units.nearbyEnemies(skat.team, skat.x, skat.y, skat.hitSize + 5, enemy => {
@@ -16,12 +16,12 @@ function SkatBiteAbilityLambda(damage_, reload_) {
 				let angleDist = Math.abs(angle - skat.rotation);
 				
 				/*two checks for situations like "angle = 350, rotation = 10"*/
-				if (angleDist < 30 || 360 - angleDist < 30) {
+				if (angleDist < this.angle || 360 - angleDist < this.angle) {
 					enemy.damage(this.damage);
 					this.effect.at(enemy.x, enemy.y, angle);
-					skat.heal(this.damage / 4);
+					skat.heal(this.damage / 5);
 					
-					if (Mathf.chance(0.2)) enemy.apply(StatusEffects.unmoving, 60);
+					if (Mathf.chance(0.07)) enemy.apply(StatusEffects.unmoving, 100);
 				}
 			});
 		}
@@ -30,28 +30,28 @@ function SkatBiteAbilityLambda(damage_, reload_) {
 
 var CKAT = extendContent(UnitType, "skat", {
 	type: "ground",
-	speed: 1.3,
+	speed: 1.2,
 	hitSize: 64,
 	canBoost: false,
 	canDrown: false, /*strong скат can't drown*/
-	health: 621,
+	health: 800,
 	buildSpeed: 0,
-	armor: 20,
+	armor: 10,
 	legLength: 0, /*ehhhhhhh*/
 
-	abilities: new Seq([SkatBiteAbilityLambda(20, 60 / 8)]),
+	abilities: new Seq([SkatBiteAbilityLambda(20, 60 / 8, 80)]),
 	
 	research: {
 		parent: UnitTypes.dagger,
 		requirements: ItemStack.with(
-			Items.graphite, 3000,
+			Items.graphite, 900,
 			Items.silicon, 3000,
-			Items.titanium, 3000, 
+			Items.titanium, 2700, 
 			Items.metaglass, 600
 		)
 	},
 	
-	/*скат gets faster when travels on water: x1.5 speed on normal water, x2.25 on deep*/
+	/*скат gets faster when travels on water*/
 	update(skat) {
 		this.super$update(skat);
 		
@@ -60,7 +60,7 @@ var CKAT = extendContent(UnitType, "skat", {
 			let floor = tile.floor();
 			if (floor.liquidDrop == Liquids.water) {
 				/*Dividing by tile's speed multi nivilates the speed multi of the tile*/
-				skat.speedMultiplier *= 1.5 * (floor.isDeep() ? 1.5 : 1) / floor.speedMultiplier;
+				skat.speedMultiplier *= 1.3 * (floor.isDeep() ? 1.5 : 1) / floor.speedMultiplier;
 			}
 		}
 	}
@@ -75,9 +75,9 @@ CKAT.defaultController = () => extend(GroundAI, {});
 
 Blocks.navalFactory.plans.add(
 	new UnitFactory.UnitPlan(CKAT, 60*30, ItemStack.with(
-		Items.graphite, 100, 
+		Items.graphite, 30, 
 		Items.silicon, 100,
-		Items.titanium, 100,
+		Items.titanium, 90,
 		Items.metaglass, 20
 	))
 );
