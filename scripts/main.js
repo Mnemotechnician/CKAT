2cd64750715da07a1c85ae35f49f3555c8af5fb6
MHeMoTexHuK <daytimer253@gmail.com>
2021-08-31 19:48:17 +0300

    somehow this even works. compete script structure rework, added sprites for t1 and t2

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,8 @@
-/*скат bite ability, damages any enemies infront of it and randomly applies unmoving effect to units*/
-function SkatBiteAbilityLambda(damage_, reload_, angle_) {
-	return extendContent(Ability, {
-		damage: damage_,
-		reload: reload_,
-		angle: angle_ / 2,
-		reloadTimer: 0,
-		
-		effect: Fx.generate,
-		
-		update(skat) {
-			if ((this.reloadTimer += Time.delta) < this.reload) return;
-			this.reloadTimer = 0; 
-			
-			Units.nearbyEnemies(skat.team, skat.x, skat.y, skat.hitSize + 5, enemy => this.attackIfInfront(skat, enemy));
-			Units.nearbyBuildings(skat.x, skat.y, skat.hitSize + 5, enemy => { if (enemy.team != skat.team) this.attackIfInfront(skat, enemy) });
-		},
-		
-		attackIfInfront(skat, enemy) {
-			let angle = skat.angleTo(enemy.x, enemy.y);
-			let angleDist = Math.abs(angle - skat.rotation);
-			
-			/*two checks for situations like "angle = 350, rotation = 10"*/
-			if (angleDist < this.angle || 360 - angleDist < this.angle) {
-				enemy.damage(this.damage);
-				this.effect.at(enemy.x, enemy.y, angle);
-				skat.heal(this.damage / 5);
-				
-				if (Mathf.chance(0.07) && enemy instanceof Unit) enemy.apply(StatusEffects.unmoving, 100);
-			}
-		}
-	});
-};
+const Ability = require("abilities");
+const AI = require("AIs");
 
 /*скат unit itself*/
-var CKAT = extendContent(UnitType, "skat", {
+const CKAT = extendContent(UnitType, "skat-t3", {
 	type: "ground",
 	speed: 1.2,
 	hitSize: 64,
@@ -47,7 +16,10 @@ var CKAT = extendContent(UnitType, "skat", {
 	mechStride: 0,
 	mechStepShake: 0,
 	
-	abilities: new Seq([SkatBiteAbilityLambda(20, 60 / 8, 80)]),
+	abilities: new Seq([
+		Ability.bite(20, 60 / 8, 80, 5),
+		Ability.swim(1.35)
+	]),
 	
 	research: {
 		parent: UnitTypes.dagger,
@@ -57,42 +29,10 @@ var CKAT = extendContent(UnitType, "skat", {
 			Items.titanium, 2700, 
 			Items.metaglass, 600
 		)
-	},
-	
-	/*скат gets faster when travels on water*/
-	update(skat) {
-		this.super$update(skat);
-		
-		let tile = Vars.world.tile(skat.x / 8, skat.y / 8);
-		if (tile != null) {
-			let floor = tile.floor();
-			if (floor.liquidDrop == Liquids.water) {
-				/*Dividing by tile's speed multi nivilates the speed multi of the tile*/
-				skat.speedMultiplier *= 1.3 * (floor.isDeep() ? 1.5 : 1) / floor.speedMultiplier;
-			}
-		}
 	}
 });
-
 CKAT.constructor = () => extend(MechUnit, {});
-/*Custom скат ai: rams nearby units and buildings, travels to the drop zone if there's none*/
-CKAT.defaultController = () => extend(AIController, {
-	updateMovement() {
-		let skat = this.unit;
-		let e = Units.closestTarget(skat.team, skat.x, skat.y, 160, e => true);
-		
-		if (e != null) {
-			this.target = e;
-			this.moveTo(e, 6);
-		} else {
-			let spawner = this.getClosestSpawner();
-			let move = spawner != null && !skat.within(spawner, Vars.state.rules.dropZoneRadius + skat.hitSize * 1.5);
-			if (move) this.pathfind(Pathfinder.fieldCore);
-		}
-		
-		if (this.faceTarget) this.faceTarget();
-	}
-});
+CKAT.defaultController = AI.rammer;
 
 Blocks.navalFactory.plans.add(
 	new UnitFactory.UnitPlan(CKAT, 60*30, ItemStack.with(
