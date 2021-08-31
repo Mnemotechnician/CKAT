cfa7db86f04983fd1fa4078695bffc686604040c
MHeMoTexHuK <daytimer253@gmail.com>
2021-08-31 11:35:31 +0300

    скат now has it's very own AI, hooray!

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
+/*скат bite ability, damages any enemies infront of it and randomly applies unmoving effect to units*/
 function SkatBiteAbilityLambda(damage_, reload_, angle_) {
-	return extend(Ability, {
+	return extendContent(Ability, {
 		damage: damage_,
 		reload: reload_,
 		angle: angle_ / 2,
@@ -11,23 +12,27 @@ function SkatBiteAbilityLambda(damage_, reload_, angle_) {
 			if ((this.reloadTimer += Time.delta) < this.reload) return;
 			this.reloadTimer = 0; 
 			
-			Units.nearbyEnemies(skat.team, skat.x, skat.y, skat.hitSize + 5, enemy => {
-				let angle = skat.angleTo(enemy.x, enemy.y);
-				let angleDist = Math.abs(angle - skat.rotation);
+			Units.nearbyEnemies(skat.team, skat.x, skat.y, skat.hitSize + 5, enemy => this.attackIfInfront(skat, enemy));
+			Units.nearbyBuildings(skat.x, skat.y, skat.hitSize + 5, enemy => { if (enemy.team != skat.team) this.attackIfInfront(skat, enemy) });
+		},
+		
+		attackIfInfront(skat, enemy) {
+			let angle = skat.angleTo(enemy.x, enemy.y);
+			let angleDist = Math.abs(angle - skat.rotation);
+			
+			/*two checks for situations like "angle = 350, rotation = 10"*/
+			if (angleDist < this.angle || 360 - angleDist < this.angle) {
+				enemy.damage(this.damage);
+				this.effect.at(enemy.x, enemy.y, angle);
+				skat.heal(this.damage / 5);
 				
-				/*two checks for situations like "angle = 350, rotation = 10"*/
-				if (angleDist < this.angle || 360 - angleDist < this.angle) {
-					enemy.damage(this.damage);
-					this.effect.at(enemy.x, enemy.y, angle);
-					skat.heal(this.damage / 5);
-					
-					if (Mathf.chance(0.07)) enemy.apply(StatusEffects.unmoving, 100);
-				}
-			});
+				if (Mathf.chance(0.07) && enemy instanceof Unit) enemy.apply(StatusEffects.unmoving, 100);
+			}
 		}
 	});
 };
 
+/*скат unit itself*/
 var CKAT = extendContent(UnitType, "skat", {
 	type: "ground",
 	speed: 1.2,
@@ -37,8 +42,11 @@ var CKAT = extendContent(UnitType, "skat", {
 	health: 800,
 	buildSpeed: 0,
 	armor: 10,
+	
 	legLength: 0, /*ehhhhhhh*/
-
+	mechStride: 0,
+	mechStepShake: 0,
+	
 	abilities: new Seq([SkatBiteAbilityLambda(20, 60 / 8, 80)]),
 	
 	research: {
@@ -66,12 +74,25 @@ var CKAT = extendContent(UnitType, "skat", {
 	}
 });
 
-/*We don't want скат to have legs, do we?*/
-CKAT.mechStride = 0;
-CKAT.mechStepShake = 0;
-
 CKAT.constructor = () => extend(MechUnit, {});
-CKAT.defaultController = () => extend(GroundAI, {});
+/*Custom скат ai: rams nearby units and buildings, travels to the drop zone if there's none*/
+CKAT.defaultController = () => extend(AIController, {
+	updateMovement() {
+		let skat = this.unit;
+		let e = Units.closestTarget(skat.team, skat.x, skat.y, 160, e => true);
+		
+		if (e != null) {
+			this.target = e;
+			this.moveTo(e, 6);
+		} else {
+			let spawner = this.getClosestSpawner();
+			let move = spawner != null && !skat.within(spawner, Vars.state.rules.dropZoneRadius + skat.hitSize * 1.5);
+			if (move) this.pathfind(Pathfinder.fieldCore);
+		}
+		
+		this.faceTarget();
+	}
+});
 
 Blocks.navalFactory.plans.add(
 	new UnitFactory.UnitPlan(CKAT, 60*30, ItemStack.with(
