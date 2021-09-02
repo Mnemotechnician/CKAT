e9fe4b316e13ad13451bda56956a88d4f6fbd9f3
MHeMoTexHuK <daytimer253@gmail.com>
2021-09-02 14:28:28 +0300

    rally

--- scripts/AIs.js
+++ scripts/AIs.js
@@ -1,8 +1,16 @@
 const RammerAI = () => extend(AIController, {
 	updateMovement() {
 		let skat = this.unit;
-		let e = Units.closestTarget(skat.team, skat.x, skat.y, 160, e => true);
 		
+		if (this.command() == UnitCommand.rally) {
+            this.target = this.targetFlag(skat.x, skat.y, BlockFlag.rally, false);
+            if(this.target != null && !skat.within(this.target, 70)){
+				this.pathfind(Pathfinder.fieldRally);
+				return;
+			}
+		}
+		
+		let e = Units.closestTarget(skat.team, skat.x, skat.y, 160, e => true);
 		if (e != null) {
 			this.target = e;
 			this.moveTo(e, 6);
