f9cce6e1a9e077bbed8572bdd3dfa91728d5b41d
MHeMoTexHuK <daytimer253@gmail.com>
2021-11-22 18:53:15 +0300

    AAAAAAAA

--- src/stingray/entities/StingrayUnit.kt
+++ src/stingray/entities/StingrayUnit.kt
@@ -20,7 +20,7 @@ open class StingrayUnit : mindustry.gen.MechUnit() {
 		val type = this.type;
 		if (type !is StingrayUnitType) {
 			Log.warn("not a stingray, get out of my land!");
-			Log.warn("it is ${type::class}, a descendant of ${type::class.getSuperclass}")
+			Log.warn("it is ${type::class}, a descendant of ${type::class.getSuperclass()}")
 		} else {
 			type.behavior.each {
 				val copy = it.copy();
