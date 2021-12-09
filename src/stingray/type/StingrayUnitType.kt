19228149c0d7be3452ca6395812fd6229b6737ce
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-09 19:52:40 +0300

    Aaaaa

--- src/stingray/type/StingrayUnitType.kt
+++ src/stingray/type/StingrayUnitType.kt
@@ -19,7 +19,7 @@ open class StingrayUnitType : UnitType {
 	
 	constructor(name: String) : super(name) {
 		EntityMapping.register(name, ::StingrayUnit)
-		constructor = EntityMapping.map(name)
+		constructor = ::StingrayUnit
 		defaultController = Prov { StingrayAI() };
 	}
 	
