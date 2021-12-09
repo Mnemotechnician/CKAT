9fb3aca474d79385d0347ff00ec4886ee31822ed
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-09 22:17:53 +0300

    stingrays can now be produced via a payload source

--- src/stingray/StingrayMod.kt
+++ src/stingray/StingrayMod.kt
@@ -7,7 +7,6 @@ import mindustry.game.*;
 import mindustry.mod.*;
 import mindustry.type.*;
 import mindustry.content.*;
-import mindustry.world.blocks.units.*;
 
 import stingray.content.*;
 
@@ -24,12 +23,12 @@ open class StingrayMod : Mod() {
 		StingrayUnitTypes().load();
 		StingrayTechTree().load();
 		
-		//Add construction plans, idk where to put it other than here
-		val cost = ItemStack.with(Items.graphite, 20, Items.silicon, 35, Items.titanium, 10, Items.metaglass, 10);
-		(Blocks.navalFactory as UnitFactory).plans.add(UnitFactory.UnitPlan(StingrayUnitTypes.urotry, 60 * 30f, cost));
-		
-		(Blocks.additiveReconstructor as Reconstructor).addUpgrade(StingrayUnitTypes.urotry, StingrayUnitTypes.mylio);
-		(Blocks.multiplicativeReconstructor as Reconstructor).addUpgrade(StingrayUnitTypes.mylio, StingrayUnitTypes.undulate);
-		(Blocks.exponentialReconstructor as Reconstructor).addUpgrade(StingrayUnitTypes.undulate, StingrayUnitTypes.dasya);
+		//make it possible to produce stingrays via a payload source
+		Blocks.payloadSource.config(StingrayUnitType::class.java) { build: Building, unit: StingrayUnitType ->
+			build.unit = unit;
+			build.block = null;
+			build.payload = null;
+			build.scl = 0f
+		}
 	}
 }
--- src/stingray/content/StingrayTechTree.kt
+++ src/stingray/content/StingrayTechTree.kt
@@ -4,6 +4,7 @@ import mindustry.*;
 import mindustry.type.*;
 import mindustry.ctype.*;
 import mindustry.content.*;
+import mindustry.world.blocks.units.*;
 
 import stingray.content.*;
 
@@ -14,6 +15,14 @@ open class StingrayTechTree : ContentList {
 		unitTech(StingrayUnitTypes.mylio, StingrayUnitTypes.urotry);
 		unitTech(StingrayUnitTypes.undulate, StingrayUnitTypes.mylio);
 		unitTech(StingrayUnitTypes.dasya, StingrayUnitTypes.undulate);
+		
+		//Add construction plans
+		val cost = ItemStack.with(Items.graphite, 20, Items.silicon, 35, Items.titanium, 10, Items.metaglass, 10);
+		(Blocks.navalFactory as UnitFactory).plans.add(UnitFactory.UnitPlan(StingrayUnitTypes.urotry, 60 * 30f, cost));
+		
+		(Blocks.additiveReconstructor as Reconstructor).addUpgrade(StingrayUnitTypes.urotry, StingrayUnitTypes.mylio);
+		(Blocks.multiplicativeReconstructor as Reconstructor).addUpgrade(StingrayUnitTypes.mylio, StingrayUnitTypes.undulate);
+		(Blocks.exponentialReconstructor as Reconstructor).addUpgrade(StingrayUnitTypes.undulate, StingrayUnitTypes.dasya);
 	}
 	
 	inline fun unitTech(unit: UnitType, parent: UnitType) {
